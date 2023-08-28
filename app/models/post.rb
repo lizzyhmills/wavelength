2eada3f62d579f584161035074e1014e9b770583
Tom Fallis <tomfallis@icloud.com>
2023-08-28 15:08:21 +0100

    updated models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :favouritePosts, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   has_many :friendships_as_follower, class_name: "Friendship", foreign_key: :follower_id
   has_many :friendships_as_followee, class_name: "Friendship", foreign_key: :followee_id
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :favouritePosts, dependent: :destroy
 end
