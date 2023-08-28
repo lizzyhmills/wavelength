9fa4f7764b57e8458548bb1dbc9a01804a514cd8
Tom Fallis <tomfallis@icloud.com>
2023-08-28 16:23:38 +0100

    making posts routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :posts
 end
