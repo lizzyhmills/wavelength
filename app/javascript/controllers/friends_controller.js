d432fbc7e3f80e38e1ddd501a68906fcfc2797d4
Tom Fallis <tomfallis@icloud.com>
2023-08-29 23:19:44 +0100

    nudge function working

--- app/javascript/controllers/friends_controller.js
+++ app/javascript/controllers/friends_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="friends"
 export default class extends Controller {
-  static targets = ["suggestions", "friends", "requests"]
+  static targets = ["suggestions", "friends", "requests", "nudge"]
   connect() {
     console.log('hello from friends controller')
     console.log(this.suggestionsTarget)
@@ -24,6 +24,13 @@ export default class extends Controller {
     this.requestsTarget.classList.remove('d-none')
     this.friendsTarget.classList.add('d-none')
     this.suggestionsTarget.classList.add('d-none')
+  }
+
+  nudge(event) {
+    event.preventDefault()
+    this.nudgeTarget.innerText = "nudged!"
+    this.nudgeTarget.classList.remove('small-button-pink')
+    this.nudgeTarget.classList.add('small-button-green')
   }
 
 }
