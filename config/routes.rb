12da4d6c97478eafa3e6a464b12461089d73dd42
barkhadleyusuf <barkhadoyusuf@gmail.com>
2023-08-31 12:37:54 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'friendships/create'
-  get 'friendships/destroy'
-  get 'favourite_posts/create'
-  get 'favourite_posts/destroy'
-  get 'likes/create'
-  get 'likes/destroy'
-  get 'comments/create'
-  get 'comments/update'
-  get 'comments/destroy'
+  get "friendships/create"
+  get "friendships/destroy"
+  get "favourite_posts/create"
+  get "favourite_posts/destroy"
+  get "likes/create"
+  get "likes/destroy"
+  get "comments/create"
+  get "comments/update"
+  get "comments/destroy"
   get "posts/index"
   get "posts/new"
   devise_for :users
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :posts, except: :show do
     resources :comments
-    resources :likes
+    resources :likes, only: [:create, :destroy]
     resources :favourite_posts
   end
 
@@ -26,7 +26,6 @@ Rails.application.routes.draw do
     resources :friendships, only: [:create]
   end
   resources :friendships, only: [:index, :destroy]
-
 
   resources :friendships, only: [] do
     member do
