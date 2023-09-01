0b27a744461dea7898b475071c1ef94dc3a98cd8
Lizzy Mills <lizzyhmills@gmail.com>
2023-09-01 11:15:24 +0100

    can now submit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @post = Post.find(params[:post_id])
     @comment.post = @post
+    @comment.user = current_user
     if @comment.save!
       redirect_to posts_path
     else
