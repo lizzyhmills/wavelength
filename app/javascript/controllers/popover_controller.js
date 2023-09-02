1a7ca1581b2554fadbfb3302d4a803ffb97922f0
Tom Fallis <tomfallis@icloud.com>
2023-09-02 13:29:37 +0100

    added feature of clicking on how many likes and it popping up with usernames of whose like the post (although I know liz you don't like the popup this so this can be removed)

---
+++ app/javascript/controllers/popover_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="popover"
+export default class extends Controller {
+  connect() {
+    new bootstrap.Popover(this.element)
+  }
+}
