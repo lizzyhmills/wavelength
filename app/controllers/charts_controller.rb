b1c483909e1fcaf9280a7412bb9d5b7505a34def
Tom Fallis <tomfallis@icloud.com>
2023-09-04 12:17:52 +0100

    added artists to charts

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,27 +1,21 @@
 class ChartsController < ApplicationController
   def index
     @posts_with_genre = Post.where.not(genre: nil)
-
     @posts_with_genre = @posts_with_genre.filter do |post|
       !post.genre.start_with?("https")
     end
-
     @genre_counts = Hash.new(0)
     @posts_with_genre.each do |post|
       @genre_counts[post.genre] += 1
     end
-
     @genre_labels = @genre_counts.keys
     @genre_data = @genre_counts.values
-
     @posts = Post.all
     @artist_counts = Hash.new(0)
     @posts.each do |post|
       @artist_counts[post.artist] += 1
     end
-
     @artist_labels = @artist_counts.keys
     @artist_data = @artist_counts.values
-
   end
 end
