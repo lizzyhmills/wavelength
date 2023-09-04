a6127eaa215b6d9c7008dd6d9897c084ce1b4d55
barkhadleyusuf <barkhadoyusuf@gmail.com>
2023-09-04 10:17:38 +0100

    Adding functionality to edit button by combining stimulus controllers

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -2,12 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="comments"
 export default class extends Controller {
-  static targets = ["commentsDiv"]
+  static targets = ["commentsDiv", "editCard"]
   connect() {
     console.log("hi")
   }
 
-  toggle() {
-    this.commentsDivTarget.classList.toggle("d-none");
+  toggleComments() {
+    this.commentsDivTarget.classList.remove("d-none");
+    this.editCardTarget.classList.add("d-none");
+  }
+
+  toggleEdit() {
+    this.editCardTarget.classList.remove("d-none");
+    this.commentsDivTarget.classList.add("d-none");
   }
 }
